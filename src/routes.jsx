1f2caf1e5536b6a9c246697077f7b754e822f58c
3mrjarrar <amrjarrar10@gmail.com>
2026-07-19 19:49:16 +0300

    basenames update

--- src/routes.jsx
+++ src/routes.jsx
@@ -21,7 +21,7 @@ const router = createBrowserRouter(
     },
   ],
   {
-    basename: '/KAFinal',
+    basename: '/KAfinal',
   }
 )
 
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
-    base: '/KAFinal/',
+    base: '/KAfinal/',
 
     plugins: [react()],
 
